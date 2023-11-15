2b5e9021d780c416514a6ae872fe795661a222a4
nono <nour98nour@gmail.com>
2023-11-15 18:20:48 +0200

    fix bugs

--- funcs3.c
+++ funcs3.c
@@ -52,6 +52,12 @@ void divi(stack_t **stack, unsigned int line_number)
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
 	(*stack)->next->n /= (*stack)->n;
 	pop_top(stack, line_number);
 }
@@ -87,6 +93,12 @@ void modu(stack_t **stack, unsigned int line_number)
 	if (!stack || !*stack || !(*stack)->next)
 	{
 		fprintf(stderr, "L%u: can't mod, stack too short\n", line_number);
+		free_stack(stack);
+		exit(EXIT_FAILURE);
+	}
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%u: division by zero\n", line_number);
 		free_stack(stack);
 		exit(EXIT_FAILURE);
 	}
